56d13aeb549e3b1c0cc476d0d586c4835af23e87
HaakonLauvrak <125738098+HaakonLauvrak@users.noreply.github.com>
2023-02-22 10:23:38 +0100

    Update proc.h

--- GitHubLab/kernel/proc.h
+++ GitHubLab/kernel/proc.h
@@ -113,6 +113,10 @@ struct proc
     int killed;           // If non-zero, have been killed
     int xstate;           // Exit status to be returned to parent's wait
     int pid;              // Process ID
+    int priority;         // Priority of the process
+    int tics;             // Number of tics run
+    struct proc *prev;    // Previous process in the queue
+    struct proc *next;    // Next process in the queue
 
     // wait_lock must be held when using this:
     struct proc *parent; // Parent process
